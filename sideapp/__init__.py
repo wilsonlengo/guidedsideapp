83cef9810c7542196b6d82934b3e49f8a79b5747
wilsonlengo <wilsonngo0911@gmail.com>
2021-10-23 09:42:38 +1100

    updated url basis

--- sideapp/__init__.py
+++ sideapp/__init__.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return "Welcome to the Sports Tracker Homepage!"
-    
+
+@app.route('/signup/')
+def sign_up():
+    return "Users will sign up here" 
+
 @app.route('/results/')
 def get_results():
     return "This will be display games that have resulted"
@@ -14,6 +18,10 @@ def get_results():
 @app.route('/result/<int:result_id>/')
 def get_specific_student(result_id):
     return f"This will be a page displaying information about a specific result {result_id}
+
+@app.route('/news/')
+def get_news():
+    return "This page will display recent news articles"
 
 @app.route('/calc/<int:f_num>/<string:operator>/<int:s_num>/')
 def calc(f_num, operator, s_num):
